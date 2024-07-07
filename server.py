1119b6d041ccf74f0c671faa800c1c21f3915232
Artem Yatsenko <yatsenkoa2020@gmail.com>
2024-07-07 20:48:56 +0000

    first commit

---
+++ server.py
@@ -0,0 +1,162 @@
+# JAX DEPS
+import simpler_env
+from simpler_env.utils.env.observation_utils import get_image_from_maniskill2_obs_dict
+import mediapy
+import sapien.core as sapien
+import os
+
+# FASTAPI DEPS
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import StreamingResponse
+from typing import List
+import numpy as np
+import cv2
+import io
+from simpler_env.policies.rt1.rt1_model import RT1Inference
+import subprocess
+
+import absl.logging
+absl.logging.set_verbosity(absl.logging.ERROR)
+
+RT_1_CHECKPOINTS = {
+    "rt_1_x": "rt_1_x_tf_trained_for_002272480_step",
+    "rt_1_400k": "rt_1_tf_trained_for_000400120",
+    "rt_1_58k": "rt_1_tf_trained_for_000058240",
+    "rt_1_1k": "rt_1_tf_trained_for_000001120",
+}
+
+def run_cmd(cmd:str ):
+    proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    tmp = proc.stdout.read()
+    return tmp
+
+def get_rt_1_checkpoint(name, ckpt_dir="./SimplerEnv/checkpoints"):
+  assert name in RT_1_CHECKPOINTS, name
+  ckpt_name = RT_1_CHECKPOINTS[name]
+  ckpt_path = os.path.join(ckpt_dir, ckpt_name)
+  if not os.path.exists(ckpt_path):
+    if name == "rt_1_x":
+      run_cmd(f"gsutil -m cp -r gs://gdm-robotics-open-x-embodiment/open_x_embodiment_and_rt_x_oss/{ckpt_name}.zip {ckpt_dir}")
+      run_cmd(f"unzip {ckpt_dir}/{ckpt_name}.zip -d {ckpt_dir}")
+    else:
+      run_cmd(f"gsutil -m cp -r gs://gdm-robotics-open-x-embodiment/open_x_embodiment_and_rt_x_oss/{ckpt_name} {ckpt_dir}")
+  return ckpt_path
+
+BASE_PATH="/workspace/videos"
+
+app = FastAPI()
+
+model_names = [
+    "rt_1_x",
+    "rt_1_400k",
+    "rt_1_58k",
+    "rt_1_1k",
+    "octo_base",
+    "octo_small"
+]
+
+policy_setup = "google_robot"
+
+
+model_names = ["rt_1_x", "rt_1_400k", "rt_1_58k", "rt_1_1k"]#, "octo-base", "octo-small"]
+
+ckpt_path = get_rt_1_checkpoint("rt_1_x")
+rt_1_x_model = RT1Inference(saved_model_path=ckpt_path, policy_setup=policy_setup)
+print("loaded rt_1_x")
+
+"""
+ckpt_path = get_rt_1_checkpoint("rt_1_400k")
+rt_1_400k_model = RT1Inference(saved_model_path=ckpt_path, policy_setup=policy_setup)
+print("loaded rt_1_400k")
+
+ckpt_path = get_rt_1_checkpoint("rt_1_58k")
+rt_1_58k_model = RT1Inference(saved_model_path=ckpt_path, policy_setup=policy_setup)
+print("loaded rt_1_58k")
+
+ckpt_path = get_rt_1_checkpoint("rt_1_1k")
+rt_1_1k_model = RT1Inference(saved_model_path=ckpt_path, policy_setup=policy_setup)
+print("loaded rt_1_1k")
+"""
+
+
+def setup_env(env_name: str, model_name: str):
+    # @title Select your model and environment
+    
+    task_name = "google_robot_pick_coke_can"  # @param ["google_robot_pick_coke_can", "google_robot_move_near", "google_robot_open_drawer", "google_robot_close_drawer", "widowx_spoon_on_towel", "widowx_carrot_on_plate", "widowx_stack_cube", "widowx_put_eggplant_in_basket"]
+    
+    if 'env' in locals():
+      print("Closing existing env")
+      env.close()
+      del env
+    env = simpler_env.make(task_name)
+    
+    # Note: we turned off the denoiser as the colab kernel will crash if it's turned on
+    # To use the denoiser, please git clone our SIMPLER environments
+    # and perform evaluations locally.
+    sapien.render_config.rt_use_denoiser = False
+    
+    obs, reset_info = env.reset()
+    instruction = env.get_language_instruction()
+
+    if "google" in task_name:
+      policy_setup = "google_robot"
+    else:
+      policy_setup = "widowx_bridge"
+        
+    return env, instruction, policy_setup
+
+def run_env(env, instruction):
+    obs, reset_info = env.reset()
+    instruction = env.get_language_instruction()
+    model.reset(instruction)
+    print(instruction)
+    
+    image = get_image_from_maniskill2_obs_dict(env, obs)  # np.ndarray of shape (H, W, 3), uint8
+    images = [image]
+    predicted_terminated, success, truncated = False, False, False
+    timestep = 0
+    while not (predicted_terminated or truncated):
+        # step the model; "raw_action" is raw model action output; "action" is the processed action to be sent into maniskill env
+        raw_action, action = model.step(image)
+        predicted_terminated = bool(action["terminate_episode"][0] > 0)
+        obs, reward, success, truncated, info = env.step(
+            np.concatenate([action["world_vector"], action["rot_axangle"], action["gripper"]])
+        )
+        print(timestep, info)
+        # update image observation
+        image = get_image_from_maniskill2_obs_dict(env, obs)
+        images.append(image)
+        timestep += 1
+    return images
+
+def generate_video_path():
+    return os.path.join(BASE_PATH, str(uuidv4()) + ".mp4")
+
+def arrays_to_video(arrays, output_path, fps=30):
+    height, width, layers = arrays[0].shape
+    size = (width, height)
+    
+    out = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, size)
+    
+    for array in arrays:
+        out.write(array)
+    out.release()
+    return 
+    
+def prompt2video(env_name: str, model_name: str):
+    env, instruction = setup_env(env_name, model_name)
+    images = run_env()
+    video_path = generate_video_path()
+    arrays_to_video(images, generate_video_path())
+    assert os.path.exists(video_path)
+    return video_path
+
+@app.post("/create-video/")
+async def create_video(env_name: str, model_name:str):
+    video_path = prompt2video(env_name, model_name)
+    logging.info(f"saved video to {video_path}")
+    return StreamingResponse(io.open(video_path, "rb"), media_type="video/mp4")
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
