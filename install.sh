c5aeba5324179d22a855e26c427c07c3428f9aa9
Artem Yatsenko <yatsenkoa2020@gmail.com>
2024-07-20 02:47:25 +0000

    add package fix

--- install.sh
+++ install.sh
@@ -1,25 +1,26 @@
 # for installation on a RUNPOD VM
 # no sudo
 
-#curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-#echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
-#apt-get update && apt-get -yqq install google-cloud-cli
-#apt-get install -yqq --no-install-recommends vulkan-tools libnvidia-gl-535 ffmpeg vim tmux
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+echo "deb https://packages.cloud.google.com/apt cloud-sdk main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+apt-get update && apt-get -yqq install google-cloud-cli
+apt-get install -yqq --no-install-recommends vulkan-tools libnvidia-gl-535 ffmpeg vim tmux
 
 # @title Install Real2Sim
-#git clone https://github.com/simpler-env/ManiSkill2_real2sim.git
-#pip install -e ./ManiSkill2_real2sim
+git clone https://github.com/simpler-env/ManiSkill2_real2sim.git
+pip install -e ./ManiSkill2_real2sim
 
-#git clone https://github.com/simpler-env/SimplerEnv.git
-#pip install -e ./SimplerEnv
-#mkdir ./SimplerEnv/checkpoints
+git clone https://github.com/simpler-env/SimplerEnv.git
+pip install -e ./SimplerEnv
+mkdir ./SimplerEnv/checkpoints
 
-#pip install --quiet tf_agents
+pip install --quiet tf_agents
 #
 git clone https://github.com/octo-models/octo
 cd ./octo && git checkout 653c54acde686fde619855f2eac0dd6edad7116b && cd ..
 pip3 install -e ./octo
 
-#pip install --quiet mediapy
+pip install --quiet mediapy
 pip3 install tensorflow-probability==0.22.1
+pip3 install --upgrade typing_extensions
 
